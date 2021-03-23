37414ed5462134ae278c0287b840667a86fba75c
JJ <jj.vega86@gmail.com>
2021-03-23 11:14:53 -0400

    Added Superhero model to app and registered in admin.py

--- comicdatabase/superheroes/models.py
+++ comicdatabase/superheroes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=30)
+    alter_ego = models.CharField(max_length=30)
+    primary_ability = models.CharField(max_length=50)
+    secondary_ability = models.CharField(max_length=50)
+    catchphrase = models.CharField(max_length=100)
+    date_introduced = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
