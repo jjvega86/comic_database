f650f4dea33932daa456dd0c6c134f4b54f2d594
JJ <jj.vega86@gmail.com>
2021-03-23 12:28:42 -0400

    Added templating logic to index, added detail function to views and template

--- comicdatabase/superheroes/views.py
+++ comicdatabase/superheroes/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import Superhero
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'superheroes/index.html')
+    all_superheros = Superhero.objects.all()
+    context = {
+        'all_superheroes': all_superheros
+    }
+    return render(request, 'superheroes/index.html', context)
+
+
+def detail(request, superhero_id):
+    single_superhero = Superhero.objects.get(pk=superhero_id)
+    context = {
+        'single_superhero': single_superhero
+    }
+    return render(request, 'superheroes/detail.html', context)
